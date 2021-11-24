0357a3c4da4a39ebf2f62348c752a4eb26ebaf82
hiroaki-com <comukichi@gmail.com>
2021-11-24 22:58:23 +0900

    bf-bootstrap

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,11 @@ module Myapp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    # ********** 以下を追加 **********
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+    # ********** 以上を追加 **********
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
