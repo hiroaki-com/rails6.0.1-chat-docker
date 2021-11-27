4a64f59531622a8b73903b9f1c0e2c1a4c52a56a
hiroaki-com <comukichi@gmail.com>
2021-11-27 18:28:08 +0900

    gest

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
 
   # 次の一行を追加
   has_many :messages, dependent: :destroy
+
+  # ログイんユーザー実装のため記載
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   # 次の一行を追加
   resources :messages, only: :create
   get '/show_additionally', to: 'rooms#show_additionally'
+
+  # ゲストログインユーザー機能実装のため記述
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
 end
