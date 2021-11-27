2dd78be9b50c442752483339b64541b7eb5ee25a
hiroaki-com <comukichi@gmail.com>
2021-11-27 14:12:22 +0900

    completed this app

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,16 +1,13 @@
 class RoomsController < ApplicationController
   def show
-    # 投稿一覧表示に利用
-    @messages = Message.includes(:user).order(:id)
+    # 投稿一覧表示に利用。最新の55件のみ取得する。
+    @messages = Message.includes(:user).order(:id).last(55)
     # メッセージ投稿に利用
     @message = current_user.messages.build
   end
 
-  # ********** 以下を追加 **********
   def show_additionally
-    # 追加のメッセージ50件を取得する
-    last_id = params[:oldest_message_id].to_i - 1    
+    last_id = params[:oldest_message_id].to_i - 1
     @messages = Message.includes(:user).order(:id).where(id: 1..last_id).last(50)
   end
-  # ********** 以上を追加 **********
 end
